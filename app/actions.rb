24d5adc3ebde4bc8871944830a8b3016869b41af
Marc Santos <msantor27@yahoo.com>
2017-05-26 03:55:31 +0000

    added Ruby stuff

---
+++ app/actions.rb
@@ -0,0 +1,23 @@
+def humanized_time_ago(time_ago_in_minutes)
+    if minute_num >= 60
+        "#{time_ago_in_minutes} hours ago"
+    else 
+        "#{time_ago_in_minutes} minutes ago"
+    end
+ end
+ 
+get '/' do 
+    post = {
+        username= "sharky_j"
+        avatar_url= "naserca.com/images/sharky_j.jpg"
+        photo_url= "naserca.com/images/sharky_j.jpg"
+        time_ago_in_minutes= 15
+        like_count= 0
+        comment_count= 1
+        comments=[
+            "sharky_j: Out for the long weekend...too embarassed to show y'all to the beach bod!"
+                ]
+    }
+  
+    humanized_time_ago(post[:time_ago_in_minutes])
+end
