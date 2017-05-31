431b8e79bbc953b8cd18d390624eb1c66a11a70f
Marc Santos <msantor27@yahoo.com>
2017-05-31 04:12:59 +0000

    added layout stuff

--- app/actions.rb
+++ app/actions.rb
@@ -1,23 +1,25 @@
 def humanized_time_ago(time_ago_in_minutes)
-    if minute_num >= 60
+    if time_ago_in_minutes >= 60
         "#{time_ago_in_minutes} hours ago"
     else 
         "#{time_ago_in_minutes} minutes ago"
     end
- end
+end
  
 get '/' do 
     post = {
-        username= "sharky_j"
-        avatar_url= "naserca.com/images/sharky_j.jpg"
-        photo_url= "naserca.com/images/sharky_j.jpg"
-        time_ago_in_minutes= 15
-        like_count= 0
-        comment_count= 1
-        comments=[
+        username: "sharky_j",
+        avatar_url: "naserca.com/images/sharky_j.jpg",
+        photo_url: "naserca.com/images/sharky_j.jpg",
+        time_ago_in_minutes: 15,
+        like_count: 0,
+        comment_count: 1,
+        comments:[
             "sharky_j: Out for the long weekend...too embarassed to show y'all to the beach bod!"
                 ]
+   
     }
   
-    humanized_time_ago(post[:time_ago_in_minutes])
+    
+erb :index
 end
