d2dc4cb766e6549703b1137067e591cef4d179fd
Marc Santos <msantor27@yahoo.com>
2017-06-07 03:52:37 +0000

    fixed ERB and Active Record stuff

--- app/actions.rb
+++ app/actions.rb
@@ -1,16 +1,16 @@
-def humanized_time_ago(time_ago_in_minutes)
+def humanized_time_ago (time_ago_in_minutes)
     if time_ago_in_minutes >= 60
-        "#{time_ago_in_minutes} hours ago"
+        "#{time_ago_in_minutes/60} hours ago"
     else 
         "#{time_ago_in_minutes} minutes ago"
     end
-end
- 
+end 
+
 get '/' do 
     @post_shark = {
         username: "sharky_j",
         avatar_url: "http://naserca.com/images/sharky_j.jpg",
-        photo_url:  "http://naserca.com/images/sharky_j.jpg",
+        photo_url:  "http://naserca.com/images/shark.jpg",
         humanized_time_ago: humanized_time_ago(15),
         like_count: 0,
         comment_count: 1,
@@ -28,14 +28,14 @@ get '/' do
         like_count: 0,
         comment_count: 1,
         comments:[{
-            username: "kirk_whalum"
+            username: "kirk_whalum",
             text: "#weekendvibes"
                 }]
     }
     @post_marlin = {
-        username: "marlin_peppa"
-        avatar_url: "http://naserca.com/images/marlin_peppa.jpg"
-        photo_url:  "http://naserca.com/images/marlin.jpg"
+        username: "marlin_peppa",
+        avatar_url: "http://naserca.com/images/marlin_peppa.jpg",
+        photo_url:  "http://naserca.com/images/marlin.jpg",
         humanized_time_ago: humanized_time_ago(190),
         like_count: 0,
         comment_count: 1,
@@ -45,7 +45,8 @@ get '/' do
         }]
         
     }
-[@post_shark, @post_whale, @post_marlin].t_s
+
     
+@posts=[@post_shark, @post_whale, @post_marlin]
 erb (:index)
 end
